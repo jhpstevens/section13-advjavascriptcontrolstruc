b77cd84a7ce16752a0828ca8a9eae30db4c7be10
jhpstevens <jhpstevens@gmail.com>
2022-02-04 16:53:57 +0100

    Loops

--- loops.js
+++ loops.js
@@ -1,10 +1,44 @@
+// common loop
+
 for (let i = 0; i <= 10; i++) {
   console.log(i);
 }
 
+
+// for-of loop (arrays)
 const users = ['Max' , 'Anna' , 'Joel'];
 console.log(users);
 
 for (const user of users) {
     console.log(user);
 }
+
+
+// for-in loop (objects)
+const loggedinUser = {
+  name: "Max",
+  age: 32,
+  isAdmin: true,
+};
+
+for (const propertyName in loggedinUser) {
+  console.log(propertyName);
+  console.log(loggedinUser[propertyName]);
+}
+
+
+// while loop (loop when condition is true. Stop when conditiobn is false)
+
+let counter = 10;
+
+while (counter != 0) {
+  console.log(counter);
+  counter--;
+}
+
+let isFinished = false;
+
+while (!isFinished) {
+  isFinished = confirm("Do you want to quit?");
+}
+console.log("Done!");
