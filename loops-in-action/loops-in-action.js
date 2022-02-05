e842f7333e47cf385fb862dad9600f0b6b06be65
jhpstevens <jhpstevens@gmail.com>
2022-02-05 18:29:21 +0100

    Exercise loops 2 and queryselectorAll

--- loops-in-action/loops-in-action.js
+++ loops-in-action/loops-in-action.js
@@ -1,9 +1,9 @@
 // First Example: Sum numbers
 
-const calculateSumButtonElement = document.querySelector('#calculator button');
+const calculateSumButtonElement = document.querySelector("#calculator button");
 
 function calculateSum() {
-  const userNumberInputElement = document.getElementById('user-number');
+  const userNumberInputElement = document.getElementById("user-number");
   const enteredNumber = userNumberInputElement.value;
 
   let sumUpToNumber = 0;
@@ -12,11 +12,29 @@ function calculateSum() {
     sumUpToNumber = sumUpToNumber + i;
   }
 
-  const outputResultElement = document.getElementById('calculated-sum');
+  const outputResultElement = document.getElementById("calculated-sum");
 
   outputResultElement.textContent = sumUpToNumber;
-  outputResultElement.style.display = 'block';
+  outputResultElement.style.display = "block";
   return sumUpToNumber;
 }
 
- calculateSumButtonElement.addEventListener('click', calculateSum );
+calculateSumButtonElement.addEventListener("click", calculateSum);
+
+// Second example: higlight all links
+
+const hightlightLinksButtonElement = document.querySelector(
+  "#highlight-links button"
+);
+
+function highlightLinks() {
+    const anchorElements = document.querySelectorAll('#highlight-links a');
+
+    for ( const anchorElement of anchorElements ) {
+      anchorElement.classList.add('highlight');
+    }
+
+}
+
+hightlightLinksButtonElement.addEventListener("click", highlightLinks);
+
